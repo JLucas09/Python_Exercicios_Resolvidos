05a36d4b5bf4a99ab4cb43e85dd8e5d0d12e023a
JLucas09 <87788474+JLucas09@users.noreply.github.com>
2022-07-24 17:00:30 -0300

    Update Questao_01.py

--- Questao_01.py
+++ Questao_01.py
@@ -1,8 +1,8 @@
 preco = float(input("Informe o preço do produto:"))
-desconto = float(input("Informe o desconto:"))
+desconto = float(input("Informe o percentual de desconto:"))
 
 valorDesconto = preco * (desconto / 100)
-precoFinal = preco -  valorDesconto 
+precoFinal = preco - valorDesconto
 
-print("O valor do desconto e:", valorDesconto)
+print(f"O valor do desconto e: {valorDesconto}%")
 print("O valor atual com o desconto aplicado:", precoFinal)
