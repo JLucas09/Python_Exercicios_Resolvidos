630aab77a4d8cd5c42d1cf131d651133d79b63f2
JLucas09 <87788474+JLucas09@users.noreply.github.com>
2022-07-25 10:36:21 -0300

    Update Questao_06.py

--- Questao_06.py
+++ Questao_06.py
@@ -5,7 +5,7 @@ quantMeses = int(input("Informe a quantidade de parcelas:"))
 valorParcela = valorImovel / quantMeses
 salPres = salario * 0.3
 
-if valorParcela <= salPres :
+if valorParcela <= salPres:
     print("financiamento imobiliario aprovado!")
 else:
     print("financiamento imobiliario reprovado!")
